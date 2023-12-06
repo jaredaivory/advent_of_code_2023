b224605f056590008ae6ec9fee15ef28d274adb6
jaredaivory <26067982+jaredaivory@users.noreply.github.com>
2023-12-05 23:40:12 -0500

    completed day01_part01

--- Solutions/Day_01/README.md
+++ Solutions/Day_01/README.md
@@ -15,6 +15,7 @@ As they're making the final adjustments, they discover that their **calibration 
 The newly-improved calibration document consists of lines of text; each line originally contained a specific **calibration value** that the Elves now need to recover. On each line, the calibration value can be found by combining the **first digit** and the **last digit** (in that order) to form a **single two-digit number**.
 
 For example:
+
     1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
@@ -23,3 +24,35 @@ For example:
 In this example, the calibration values of these four lines are `12, 38, 15, and 77`. Adding these together produces `142`.
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
+
+<details>
+<summary><b>Answer</b></summary>
+
+54644
+
+</details>
+
+## **Trebuchet?! | Part 2**
+
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters:
+`one`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, and `nine` also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are `29, 83, 13, 24, 42, 14, and 76`. Adding these together produces `281`.
+
+What is the sum of all of the calibration values?
+
+<details>
+<summary><b>Answer</b></summary>
+
+???
+
+</details>
---
+++ Solutions/Day_01/src/part_one.rs
@@ -0,0 +1,63 @@
+use std::fmt::Error;
+use std::io::prelude::BufRead;
+use utils::file::read_file;
+
+
+fn calibration_value(calibration: String) -> u32 {
+
+    let bytes = calibration.as_bytes();
+
+    let mut result = 0;
+
+    let zero_bound =  '0' as u8;
+    let nine_bound = '9' as u8;
+
+    for i in 0..bytes.len() {
+        if bytes[i] >= zero_bound && bytes[i] <= nine_bound {
+            result += (bytes[i] - zero_bound) as u32 * 10;
+            break;
+        }
+    }
+
+    for i in (0..bytes.len()).rev() {
+        if bytes[i] >= zero_bound && bytes[i] <= nine_bound {
+            result += (bytes[i] - zero_bound) as u32;
+            break;
+        }
+    }
+    result
+}
+
+
+pub fn start() -> Result<(), Error> {
+    let reader = read_file("solutions/day_01/input.txt").expect("Err");
+
+    let mut calibration_value_sum = 0;
+    
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                calibration_value_sum += calibration_value(line);
+            },
+            Err(error) => panic!("Problem opening the file: {:?}", error)
+        }
+    }
+
+    println!("\n\n\nAdvent Of Code - 2023 | Part 1: Calibration Sum = {}", calibration_value_sum);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::calibration_value;
+    use test_case::test_case;
+
+    #[test_case("1abc2" => 12; "test_1")]
+    #[test_case("pqr3stu8vwx" => 38; "test_2")]
+    #[test_case("a1b2c3d4e5f" => 15; "test_3")]
+    #[test_case("treb7uchet" => 77; "test_4")]
+    fn process_calibration_line(s: &str) -> u32 {
+        calibration_value(s.to_string())
+    }
+}
